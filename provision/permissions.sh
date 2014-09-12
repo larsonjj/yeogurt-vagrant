874b2bfd6a7d026147d366d3adfa0d4f6c78aed6
Jake Larson <jake.j.larson@gmail.com>
2014-09-12 11:22:02 -0500

    Update permissions.sh

--- provision/permissions.sh
+++ provision/permissions.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# Setup permissions for npm
-chown -R vagrant /home/vagrant/local/bin
-chown -R vagrant /home/vagrant/local/lib
-
 echo 'Congratulations!'
 echo 'Yeogurt Install Completed with 0 Errors.'
 echo 'Run "vagrant ssh" to start working with Yeogurt.'
